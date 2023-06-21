72551fe058b07186aae4e0d33187332a0df7a8a4
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-06-21 21:02:32 +0000

    webser default file changes

--- webserver-setup.sh
+++ webserver-setup.sh
@@ -8,6 +8,10 @@ sudo apt-get install -y php apache2
 
 sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app
 
+# Replace and make the app directory the default instead of index.html
+
+sudo sed -i 's/\/var\/www\/html/\/var\/www\/html\/app/g' /etc/apache2/sites-available/000-default.conf
+
 #Start Apache
 
 sudo service apache2 start
