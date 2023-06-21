a295e334e3879fa2cb0bbf364c103c4b32c4d977
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-06-21 19:17:18 +0000

    web-app deploying configuration

---
+++ webserver-setup.sh
@@ -0,0 +1,11 @@
+# update the apt-get cache
+sudo apt-get update
+
+# To install Php and Apache
+sudo apt-get install -y php apache2
+
+# Copy the code from source repository
+
+sudo git clone https://github.com/brikis98/php-app.git /var/www/html/app
+
+
